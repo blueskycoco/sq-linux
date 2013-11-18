2287a214abf7433626d354bb1bce902bfa0925a6
ubuntu-32 <rdp@ubuntu.(none)>
2013-11-17 17:47:50 -0800

    add env set sh

---
+++ 1.sh
@@ -0,0 +1,6 @@
+#! /bin/bash -e
+
+export CROSS_COMPILE=arm-none-linux-gnueabi-
+export PATH=$PATH:/home/rdp/work/ti/am3359/tools/
+export ARCH=arm
+export PATH=$PATH:/home/rdp/work/ti/am3359/tools/arm-2009q1/bin
